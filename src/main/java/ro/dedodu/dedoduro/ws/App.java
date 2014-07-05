1f7ae317227ff16bd2411a63f91472f418942904
daniel_frank <mitzgitari@gmail.com>
2014-07-05 13:27:51 -0300

    Commit inicial.

---
+++ src/main/java/ro/dedodu/dedoduro/ws/App.java
@@ -0,0 +1,11 @@
+package ro.dedodu.dedoduro.ws;
+
+/**
+ * Backend para as aplicações da plataforma Dedo Duro.
+ * @author Daniel R C Frank
+ * @since 05/07/2014
+ * @version 1.0.0
+ */
+public class App {
+
+}
