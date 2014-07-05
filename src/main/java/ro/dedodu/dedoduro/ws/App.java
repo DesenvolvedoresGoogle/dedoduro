4f83d1cc939a526d05f2fb44a3d5ce69e6af3c3e
daniel_frank <mitzgitari@gmail.com>
2014-07-05 13:37:45 -0300

    Configuração base do spring-boot.

--- src/main/java/ro/dedodu/dedoduro/ws/App.java
+++ src/main/java/ro/dedodu/dedoduro/ws/App.java
@@ -1,11 +1,26 @@
 package ro.dedodu.dedoduro.ws;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Backend para as aplicações da plataforma Dedo Duro.
  * @author Daniel R C Frank
  * @since 05/07/2014
  * @version 1.0.0
  */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class App {
 
+    /**
+     * Ponto de entrada.
+     * @param args Argumetos passado para a aplicação.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
