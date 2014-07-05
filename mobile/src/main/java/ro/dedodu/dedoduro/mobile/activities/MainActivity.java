de8b5d9e287d65efb562710ce57688cc83a20ca7
alexssandroreis <shagrath>
2014-07-05 15:10:12 -0300

    Add icones e acesso ao google maps

--- mobile/src/main/java/ro/dedodu/dedoduro/mobile/activities/MainActivity.java
+++ mobile/src/main/java/ro/dedodu/dedoduro/mobile/activities/MainActivity.java
@@ -1,9 +1,28 @@
 package ro.dedodu.dedoduro.mobile.activities;
 
+import android.os.Bundle;
+
 import com.actionbarsherlock.app.SherlockActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import ro.dedodu.dedoduro.mobile.R;
 
 public class MainActivity extends SherlockActivity {
 
+    private GoogleMap map;
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeMap();
+    }
 
+    private void initializeMap() {
+        MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        if (fragment != null) {
+            map = fragment.getMap();
+            map.setMyLocationEnabled(true);
+        }
+    }
 }
